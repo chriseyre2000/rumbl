805d6c8501486a936c3984d34607e402d74dcdaa
Christopher Eyre <chriseyre2000@gmail.com>
2020-12-05 16:32:01 +0000

    End of Chapter 5

--- lib/rumbl/accounts/user.ex
+++ lib/rumbl/accounts/user.ex
@@ -3,11 +3,21 @@ defmodule Rumbl.Accounts.User do
 
   import Ecto.Changeset
 
+  alias Rumbl.Accounts.Credential
+
   schema "users" do
     field :name, :string
     field :username, :string
+    has_one :credential, Credential
 
     timestamps()
+  end
+
+  def registration_changeset(user, params) do
+    user
+    |> changeset(params)
+    |> cast_assoc(:credential, with: &Credential.changeset/2, required: true)
+    |> unique_constraint(:username)
   end
 
   def changeset(user, attrs) do
--- mix.exs
+++ mix.exs
@@ -10,7 +10,8 @@ defmodule Rumbl.MixProject do
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      xref: [exclude: Comeonin.Pbkdf2]
     ]
   end
 
@@ -44,7 +45,9 @@ defmodule Rumbl.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:comeonin, "~> 4.1"},
+      {:pbkdf2_elixir, "~> 0.12"}
     ]
   end
 
